23f60a0bf464e4024f72fa05b132a123101e458f
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-07-17 14:14:55 -0300

    comentário

--- src/components/CardAdicionar/index.jsx
+++ src/components/CardAdicionar/index.jsx
@@ -1,3 +1,10 @@
+/*Vamos criar um componente para criar uma tarefa. Ele deve ser chamado de CardAdicionar. 
+Siga os passos do checklist abaixo para finalizar a tarefa.
+1 - Crie um useState para guardar o texto da tarefa;
+2 - Crie um input do tipo text que será usado para receber o texto da tarefa a ser criada;
+3 - crie um botão para chamar a função de adicionar a tarefa;
+4 - faça o botão chamar a função de adicionar tarefa*/
+
 import { useState } from "react"
 
 function CardAdicionar(props){
